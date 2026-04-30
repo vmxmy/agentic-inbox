251164a18a2dcaba60fba60c12bca6537582e900
blueyang <blueyang@gmail.com>
2026-04-30 12:14:20 +0800

    fix(AgentSidebar): restore Loader import removed during A2 refactor

--- app/components/AgentSidebar.tsx
+++ app/components/AgentSidebar.tsx
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
-import { Button } from "@cloudflare/kumo";
+import { Button, Loader } from "@cloudflare/kumo";
 import { ChatCircleIcon, PlugsIcon, SidebarSimpleIcon } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
 import { useUIStore } from "~/hooks/useUIStore";
